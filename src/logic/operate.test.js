c7e39f7c33a373399348d61ba750b54ad9874efc
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-18 16:06:31 +0100

    Add substract and division operation tests

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,11 +1,19 @@
 import operate from './operate';
 
-describe('test operations', () => {
+describe('test operation', () => {
   test('add operations', () => {
     expect(operate(2, 2, '+')).toBe('4');
   });
 
+  test('substract operation', () => {
+    expect(operate(2, 4, '-')).toBe('-2');
+  });
+
   test('multiply operation', () => {
     expect(operate(2, 2, 'x')).toBe('4');
+  });
+
+  test('division operation', () => {
+    expect(operate(8, 2, '÷')).toBe('4');
   });
 });
